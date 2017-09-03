3d01b408c0ec2682c40fc98ae5c330dc4e02179e
ZakkZakk <ZakkZakk@Zakk>
2017-09-03 15:20:21 +0300

    Start

---
+++ TimeManagement2/src/main/java/Zakk/TimeManagement2/App.java
@@ -0,0 +1,13 @@
+package Zakk.TimeManagement2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
